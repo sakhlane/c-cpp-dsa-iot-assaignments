63cd899dbf92c36994d7fc0de9b4cc6ad76171cc
sakhlane <107583915+sakhlane@users.noreply.github.com>
2022-07-16 23:26:00 +0500

    Update 1st_assaignment.c

--- 1st_assaignment.c
+++ 1st_assaignment.c
@@ -1,98 +1,98 @@
 // 1> WAP a program to print Hello Students on the screen
 
-                // #include<stdio.h>
-                // int main(){
-                //     printf("Hello Students");
-                //     return 0;
-                // }
+                 #include<stdio.h>
+                 int main(){
+                    printf("Hello Students");
+                     return 0;
+                 }
 
 // 2> Wap to print hello in the first line and students in the second line
-            // #include<stdio.h>
-            // int main(){
-            //     printf("Hello\nStudents");
-            //     return 0;
-            // }
+             #include<stdio.h>
+             int main(){
+                 printf("Hello\nStudents");
+                 return 0;
+             }
 
 // 3> wap to print "MySirG" on the screen
-                // #include<stdio.h>
-                // int main(){
-                //     printf("\"MySirG\"");
-                //     return 0;
-                // }
+                 #include<stdio.h>
+                 int main(){
+                     printf("\"MySirG\"");
+                     return 0;
+                 }
 
 // 4> wap to find the area of a circle take radius of the circle from the user as input and prints the result as given below format
-        //  #include<stdio.h>
-        //  int main(){
-        //     int radius;
-        //      float area;
-        //     printf("enter radius of a circle\n");
-        //     scanf("%d",&radius);
-        //      area=3.14*radius*radius;
-        //      printf("the area of circle is %f and having the radius %d"area,radius);
-        //      return 0;
-        //  }
+          #include<stdio.h>
+          int main(){
+             int radius;
+              float area;
+             printf("enter radius of a circle\n");
+             scanf("%d",&radius);
+              area=3.14*radius*radius;
+              printf("the area of circle is %f and having the radius %d"area,radius);
+              return 0;
+          }
  
 //  5> wap to calculate length of a string using printf function
-                // #include<stdio.h>
-                // int main(){
-                //     int length_of_string;
-                //     length_of_string = printf("hello");
-                //     printf("%d",length_of_string);
-                //     return 0;
-                // }
+                 #include<stdio.h>
+                 int main(){
+                     int length_of_string;
+                     length_of_string = printf("hello");
+                     printf("%d",length_of_string);
+                     return 0;
+                 }
 
 // 6> wap to print name of the user in double quotes
-                // #include<stdio.h>
-                // int main(){
-                //     printf("Hello,\"Amit kumar \" ");
-                //     return 0;
-                // }
+                 #include<stdio.h>
+                 int main(){
+                     printf("Hello,\"Amit kumar \" ");
+                     return 0;
+                 }
 
 // 7> wap to print "%d" on the screen
-            // #include<stdio.h>
-            // int main(){
-            //     printf("%%d");
-            //     return 0;
-            // }
+             #include<stdio.h>
+               int main(){
+                 printf("%%d");
+                 return 0;
+             }
 
 // 8> wap to print "\n " on the screen
-            // #include<stdio.h>
-            // int main(){
-            //     printf("\\n");
-            //     return 0;
-            // }
+             #include<stdio.h>
+             int main(){
+                 printf("\\n");
+                 return 0;
+             }
 
 // 9> wap to print "\\ " on the screen 
-            // #include<stdio.h>
-            // int main(){
-            //     printf("\\\\");
-            //     return 0;
-            // }
+             #include<stdio.h>
+             int main(){
+                 printf("\\\\");
+                 return 0;
+             }
 
 // 10> wap to take date as an input in below given format and convert the date format and display the result as giben below format 
     // user input date format => "DD/MM/YYYY" (27/11/2022)
     //    output format => "Day-DD/ month-MM/ year-YYYY"
-                // (day-27/ month-7/ year-2022);
-                // #include<stdio.h>
-                // int main(){
-                //     int day, month , year;
-                //     printf("\n enter the date in DD/MM/YYYY format");
-                //     scanf("%d/%d/%d",&day,&month,&year);
-                //     printf("Day-%d/Month-%d/Year-%d",day,month,year);
-                //     return 0;
-                // }
+                 (day-27/ month-7/ year-2022);
+                 #include<stdio.h>
+                 int main(){
+                     int day, month , year;
+                    printf("\n enter the date in DD/MM/YYYY format");
+                     scanf("%d/%d/%d",&day,&month,&year);
+                     printf("Day-%d/Month-%d/Year-%d",day,month,year);
+                     return 0;
+                 }
 
 // 11> wap to take time as input in below given format and convert the time format and display the result as given below.
 //     => user input => "HH:MM".
 //     output => "HH hour and MM minutes".
-                // #include<stdio.h>
-                // int main(){
-                //     int hour, minute;
-                //     printf("enter the time in HH:MM format\n");
-                //     scanf("%d:%d",&hour,&minute);
-                //     printf("%d hour and %d minute",hour,minute);
-                //     return 0;
-                // }
+                 #include<stdio.h>
+                 int main(){
+                     int hour, minute;
+                    printf("enter the time in HH:MM format\n");
+                    scanf("%d:%d",&hour,&minute);
+                     printf("%d hour and %d minute",hour,minute);
+                    return 0;
+                 }
 
 // 12> find output of below code.
                 #include<stdio.h>
